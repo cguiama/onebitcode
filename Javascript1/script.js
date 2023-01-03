7bf63d655f10268ae57a5b9c2f98f6991d030657
Guilherme <45200933+cguiama@users.noreply.github.com>
2023-01-03 09:46:45 -0300

    first commit

---
+++ Javascript1/script.js
@@ -0,0 +1,6 @@
+let nome = prompt("Qual seu nome?")
+let sobrenome = prompt("Qual seu sobrenome?")
+let campoDeEstudo = prompt("Qual seu campo de estudo?")
+let nascimento = prompt("Qual seu ano de nascimento?")
+
+alert("Seu nome é: " + nome + " " + sobrenome + "\nVocê estuda: " + campoDeEstudo + "\nE você nasceu no ano: " + nascimento)
---
+++ Javascript1/script2.js
@@ -0,0 +1,16 @@
+let a = prompt("primeiro numero?")
+let b = prompt("segundo numero?")
+
+
+let somaCorrigida = parseInt(a) + parseInt(b);
+
+
+let soma = parseInt(a) + parseInt(b)
+let subtrai = parseInt(a) - parseInt(b);
+let divide = parseInt(a) / parseInt(b);
+let mutiplica = parseInt(a) * parseInt(b);
+
+alert("Soma:" + soma + "\nSubtração: " + subtrai + "\nDivisão: " + divide + "\nMutiplição: " + mutiplica);
+
+
+
