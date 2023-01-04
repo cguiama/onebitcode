71c83f024066b6144c133187e9e5d31c4f3b103b
Guilherme <45200933+cguiama@users.noreply.github.com>
2023-01-04 11:34:46 -0300

    corrigir pastas

--- Javascript1/3 - estrutura de dados/arrays/script2.js
+++ Javascript1/3 - estrutura de dados/arrays/script2.js
@@ -6,23 +6,23 @@ O programa deve iniciar mostrando na tela um menu interativo contendo as cartas 
 nome da carta e adicioná-la no topo do baralho. 
 
 Ao escolher “Puxar uma carta”, o programa deve retirar a carta do topo do baralho e mostrar na tela o nome da carta puxada.
- 
+
 O programa só deve ser encerrado ao escolher a opção de “Sair”, caso contrário deve voltar ao menu.
 
 */
 
-let cartas = []
+let baralho = ["7 de paus", "3 de ouro", "Ás de copas"]
 let opcao = ""
 
 do {
-  opcao = prompt("As cartas no baralho são: " + cartas + "\n\nDigite a opção desejada\n\n1- Adicionar uma carta\n2- Puxar uma carta\n3- Sair")
+  opcao = prompt("O baralho tem " + baralho.length + " cartas." + "\n\nDigite a opção desejada\n\n1- Adicionar uma carta\n2- Puxar uma carta\n3- Sair")
   switch (opcao) {
     case "1":
-      cartas.unshift(prompt("Qual é a carta?"))
+      baralho.unshift(prompt("Qual é a carta?"))
       break;
     case "2":
-      if (cartas.length == 0) { alert("Não existem mais cartas no monte.") } else {
-        cartas.shift(alert("A carta retirada foi " + cartas));
+      if (baralho.length == 0) { alert("Não existem mais cartas no monte.") } else {
+        baralho.shift(alert("A carta retirada foi " + baralho[0]));
         break;
       }
     case "3":
